5f4a08cdaba0b639277531197da5475a7b01b98e
Dhara Patel <91153621+dharapatel2908@users.noreply.github.com>
2026-04-17 22:53:15 -0700

    Add: sort-an-array - submission-3

---
+++ Data Structures & Algorithms/sort-an-array/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def count_sort():
+            count = defaultdict(int)
+            minval,maxval = min(nums),max(nums)
+            for val in nums:
+                count[val] +=1
+            
+            index = 0
+            for val in range(minval, maxval+1):
+                while count[val]>0:
+                    nums[index] = val
+                    index +=1
+                    count[val] -=1
+        count_sort()
+        return nums
+            
