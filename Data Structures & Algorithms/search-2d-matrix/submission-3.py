0c5e03435e1a3a0dba76f53d40671f4c624f0788
Dhara Patel <91153621+dharapatel2908@users.noreply.github.com>
2026-04-21 00:43:00 -0700

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        #Method 1:
+        # If we directly go to the last element of the each row and compare it if the target is smaller then only search that row or else skip it.
+        # rows, cols = len(matrix), len(matrix[0])
+        # r,c = 0,cols-1
+
+        # while r< rows and c>= 0:
+        #     if matrix[r][c]> target:
+        #         c-=1
+        #     elif matrix[r][c]< target:
+        #         r+=1
+        #     else:
+        #         return True
+        # return False
+
+        #Method 2:
+        # If we just consider the 2D matrix as big 1D sorted array and then search the element
+
+        rows, cols = len(matrix), len(matrix[0])
+        r,c = 0,cols-1
+        left = 0
+        right = rows*cols-1
+        while left <= right:
+            middle = (right-left)//2 + left 
+            current_element=matrix[middle//cols][middle % cols]
+            if current_element == target:
+                return True
+            elif current_element> target:
+                right =middle -1
+            else:
+                left  = middle +1
+        return False
+
+
+            
