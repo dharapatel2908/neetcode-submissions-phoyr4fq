02f1bc1ef3765b10b63f1ca49418df1a699ab901
Dhara Patel <91153621+dharapatel2908@users.noreply.github.com>
2026-04-25 15:50:20 -0700

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+        components = n
+        rank = [0]*n
+        parent = list(range(n))
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x,y):
+            nonlocal components
+            rootx,rooty = find(x), find(y)
+            if rootx== rooty:
+                return False
+            elif rank[rootx] >rank[rooty]:
+                parent[rooty] = rootx
+            elif rank[rootx] <rank[rooty]:
+                parent[rootx] = rooty
+
+            else:
+                parent[rooty] = rootx
+                rank[rootx] +=1
+            components-=1
+            return True
+        for u,v in edges:
+            if not union(u,v):
+                return False
+        return components ==1 
