45e56520ddb5ffae16c190411aa766da5368781c
Dhara Patel <91153621+dharapatel2908@users.noreply.github.com>
2026-07-23 09:58:27 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+    #Ambiguity = Spacing, special characters
+        def check(c):
+            return ((c>="a"and c<="z") 
+            or (c>="A" and c<="Z")
+            or (c>="0" and c<="9"))
+
+        left = 0
+        right = len(s) -1
+        while left <= right:
+            if not check(s[left]):
+                left +=1 
+            elif not check(s[right]):
+                right -=1
+            elif s[left].lower() == s[right].lower():
+                left +=1
+                right -=1
+            else:
+                return False
+        return True
+
